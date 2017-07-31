116fb1355a7815cf9772c88cc594d663d9986208
Alexander You <alexyou8021@gmail.com>
2017-07-30 22:38:04 -0500

    added in fantasy sections

--- scrape.py
+++ scrape.py
@@ -13,6 +13,7 @@ if len(sys.argv) == 4:
 elif len(sys.argv) == 3:
     tournament = sys.argv[1] 
     teamid = sys.argv[2]
+    password = ''
 else:
     teamid = '5630483324993536'
     password = ''
@@ -80,12 +81,16 @@ sheet.write(29, 0, 'Catching %')
 sheet.write(30, 0, 'Throwaways')
 sheet.write(31, 0, 'Drops')
 
+# setting actual stats
+scores = {}
 team_stats = sorted(team_stats, key=lambda k: k['playerName'])
 for x in range(0, len(team_stats)):
     if x < 25:
         letter = chr(x + ord('B'))
     else:
         letter = 'A' + str( chr(x - 26 + ord('B')))
+
+    scores[team_stats[x]['playerName']] = letter
 
     sheet.write(0, x + 1, team_stats[x]['playerName'])
     sheet.write(1, x + 1, team_stats[x]['goals'])
@@ -107,5 +112,33 @@ for x in range(0, len(team_stats)):
     sheet.write(30, x + 1, team_stats[x]['throwaways'])
     sheet.write(31, x + 1, team_stats[x]['drops'])
     
-book.save(team_info["name"] + ".xls")
-print 'New Excel file saved.'
+file = open(team_info['name'] + "_teams.txt","r")
+fteams = [line.rstrip('\n') for line in file]
+teamno = 0
+player_count = 0
+captain = True
+for fplayer in fteams:
+    if fplayer == "":
+        fletter = chr(2 + teamno + ord('A'))
+        style = xlwt.easyxf("font: bold on; pattern: pattern solid, fore_colour cyan_ega; borders: top thin, left thin, right thin, bottom thin")
+        sheet.write(15 + player_count, 1 + teamno, 'Total', style)
+        sheet.write(15 + player_count, 2 + teamno, Formula("SUM($" + fletter + "$15:$" + fletter + "$" + str(15 + player_count)  + ")"), style)
+        teamno += 3 
+        player_count = 0
+        captain = True
+        continue
+    if captain:
+        style = xlwt.easyxf("align: horiz center;")
+        sheet.write_merge(13, 13, 1 + teamno, 2 + teamno, fplayer, style)
+        style = xlwt.easyxf("pattern: pattern solid, fore_colour cyan_ega; borders: top thin, left thin, right thin, bottom thin")
+        sheet.write(14, 1 + teamno, 'Players', style)
+        sheet.write(14, 2 + teamno, 'Points', style)
+        captain = False
+        
+    style = xlwt.easyxf("pattern: pattern solid, fore_colour cyan_ega; borders: left thin, right thin")
+    sheet.write(15 + player_count, 1 + teamno, fplayer, style)
+    sheet.write(15 + player_count, 2 + teamno, Formula(scores[fplayer] + '10'), style)
+    player_count += 1
+
+book.save(team_info['name'] + '.xls')
+print 'New Excel File Saved.'
